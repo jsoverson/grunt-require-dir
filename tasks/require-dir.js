e35814a473d0f7c80dd9a6eb912aa5ce67ed212b
Jarrod Overson <joverson@onerecovery.com>
2012-09-11 15:17:08 -0700

    cleaning up, moving helpers to lib

--- tasks/require-dir.js
+++ tasks/require-dir.js
@@ -5,84 +5,23 @@
  * Contributor: @jsoverson
  */
 
-require
+var helpers = require('./helpers');
 
 module.exports = function(grunt) {
   "use strict";
 
-  grunt.registerTask("require-dir", "Compile a directory of templates into an AMD compatible JST file", function() {
-    var config = grunt.config.get('requiredir'),
-        options = config.options;
+  grunt.registerMultiTask("require-dir", "Process a directory of AMD modules and generate an AMD compatible container", function() {
+    var config = this.data;
 
-    grunt.verbose.writeflags(options, "Options");
+    grunt.verbose.writeflags(config, "Options");
 
-    var files = grunt.file.expandFiles(config.files);
+    var files = grunt.file.expandFiles(config.src);
 
-    var output = grunt.helper('require-tree', files, options);
+    var output = helpers.requireTree(files, config);
 
     grunt.file.write(config.dest, output);
 
   });
-
-  grunt.registerHelper("require-tree", function(files, options) {
-    files = files.sort();
-
-    var fileTree = grunt.helper('directory-tree', files, options.baseDir);
-
-    var output = [
-      'define(function(require){',
-      '  return {'
-    ];
-
-    var pluginPrefix = options.plugin ? options.plugin + '!' : '';
-    var directoryPrefix = options.prefixDir || '';
-    var lastDepth = 0;
-
-    function indent(num) { return grunt.utils.repeat(num,'  ')};
-
-    deepEach(fileTree, function(val,key,parentKey,depth){
-      key = key.replace(/\.[^\.]*$/,'');
-      if (depth > lastDepth)      output.push(indent(depth) + "'" + parentKey + '\' : { ' )
-      else if (depth < lastDepth) output.push(indent(depth + 1) + '}, ' )
-      output.push(indent(depth + 1) + "'" + key + '\' : require("' + pluginPrefix + directoryPrefix + val + '"),');
-      lastDepth = depth;
-    })
-
-    output.push(indent(1) + '};')
-
-    output.push('});')
-    return output.join("\n");
-  });
-
-  grunt.registerHelper('directory-tree', function(files, baseDir){
-    var tree = {};
-    var baseDirRE = new RegExp('^' + (baseDir || ''));
-
-    files.forEach(function(val,i) {
-      var requireUrl = val.replace(baseDirRE, '');
-      var parts = requireUrl.split('/');
-      var key = tree;
-      for (var i = 0; i < parts.length - 1; i++) {
-        var dir = parts[i];
-        if (!key.hasOwnProperty(dir)) key[dir] = {};
-        key = key[dir];
-      }
-      var filename = parts[parts.length - 1];
-      if (key.hasOwnProperty(filename)) throw new Error('Directory contains both dir and file named ' + filename);
-      key[filename] = requireUrl;
-    });
-
-    return tree;
-  })
-
-  function deepEach(obj,fn,parentKey,depth){
-    depth = depth || 0;
-    grunt.utils._.each(obj,function(val,key){
-      if (typeof val === 'object')
-        deepEach(val, fn, key, depth+1);
-      else fn(val,key,parentKey,depth)
-    });
-  }
 
 };
 
